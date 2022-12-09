0e59ff856cebca99bc0aff4978472d3ea9db4b5f
ToshiroR <97269007+ToshiroR@users.noreply.github.com>
2022-12-09 17:12:00 +0000

    basic tic tac toe board created in HTML

--- script.js
+++ script.js
@@ -12,4 +12,18 @@ const gameBoard = () => {
       ['','',''],
       ['','','']
     ];
+
+    const play = (spot) => {
+      for (i = 0; i < board.length; ++i) {
+        for (j = 0; j < board.length[i]; ++j){
+          
+        }
+      }
+
+    }
 };
+
+
+const tosh = Player("tosh", "x")
+
+console.log(tosh.getChoice())
