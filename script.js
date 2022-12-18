bc2b0688a03ea8f20d452e2b9aae2485c7b55151
Toshiro <toshiro3457@gmail.com>
2022-12-18 18:40:28 -0500

    working on winning message

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-
-
-
+//   extract values from form
+//   make the gameboard interactable on click
+//   when player clicks a tile, the X or O displays in the tile
+//   switch turns after player chooses a tile
+//   make sure a tile cant be overwritten
+//   check to see if theres a winner everytime a tile is chosen
+//   if theres a winner, endgame and display who won
+//   if there is no winner when all the tiles are clicked, display a tie
