b1cc0f43319eef027bb906f0a215e917113714ab
ToshiroR <97269007+ToshiroR@users.noreply.github.com>
2023-01-14 22:50:29 +0000

    currentTurn not working right, needs more work

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const player = (() => {})();
 
-const gameboard = (() => {
-  let board = [];
+const gameboard = (() => {})();
+
+const displayController = (() => {
+  const form = document.getElementById("myForm");
+  const turnPlayer = document.getElementById("turn-player");
+  const tile = document.querySelectorAll("#tile");
+  const cellElement = document.querySelectorAll("[data-cell]");
+
   const winNum = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,18 +18,26 @@ const gameboard = (() => {
     [0, 4, 8],
     [2, 4, 6],
   ];
-})();
 
-const displayController = (() => {
-  const form = document.getElementById("myForm");
+  let playerOne = choice.value;
+  let playerTwo = "";
+  if (playerOne === "X") {
+    playerTwo = "O";
+  }
+  if (playerOne === "O") {
+    playerTwo = "X";
+  }
+  let playerTurn;
+
+  //  form Submit
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    playerTurn = false;
     turn();
     //   make form disappear after submit
     form.classList.remove("show");
   });
-  const turnPlayer = document.getElementById("turn-player");
-  const tile = document.querySelectorAll("#tile");
+
   //   make the gameboard interactable on click
   tile.forEach((cell) => {
     cell.removeEventListener("click", makeMove);
@@ -31,31 +45,15 @@ const displayController = (() => {
     cell.addEventListener("click", makeMove, { once: true });
   });
 
-  let playerOne = choice.value;
-  let playerTwo = "";
-  let playerTurn = false;
-
   function makeMove(e) {
     const cell = e.target;
-
-    if (playerOne === "X") {
-      playerTwo = "O";
-    }
-    if (playerOne === "O") {
-      playerTwo = "X";
-    }
+    const currentTurn = playerTurn ? playerOne : playerTwo;
+    console.log(currentTurn);
     //   when player clicks a tile, the X or O displays in the tile
-    if (cell.textContent === "" && !playerTurn) {
-      cell.textContent = playerOne;
-      //   switch turns after player chooses a tile
-      playerTurn = true;
-      turn();
-    }
-    if (cell.textContent === "" && playerTurn) {
-      cell.textContent = playerTwo;
-      playerTurn = false;
-      turn();
-    }
+    placeMarker(cell, currentTurn);
+    //   switch turns after player chooses a tile
+    swapTurn();
+    turn();
   }
 
   function turn() {
@@ -65,6 +63,22 @@ const displayController = (() => {
     if (playerTurn) {
       turnPlayer.textContent = `${playerTwo} Turn`;
     }
+  }
+
+  function swapTurn() {
+    playerTurn = !playerTurn;
+  }
+
+  function checkWin(currentTurn) {
+    return winNum.some((combos) => {
+      return combos.every((index) => {
+        return cellElement[index].textContent.contains(currentTurn);
+      });
+    });
+  }
+
+  function placeMarker(cell, currentTurn) {
+    cell.textContent = currentTurn;
   }
 })();
 
