ffc998e4e4ad949c23fd2f98cf5fcfdad385b0b5
ToshiroR <97269007+ToshiroR@users.noreply.github.com>
2023-01-21 20:52:29 +0000

    finished display who won or a draw

--- script.js
+++ script.js
@@ -6,19 +6,20 @@ const displayController = (() => {
   const form = document.getElementById("myForm");
   const turnPlayer = document.getElementById("turn-player");
   const tile = document.querySelectorAll("#tile");
+  const winText = document.querySelector("[win-text]");
+  const winningMessage = document.querySelector(".winning-message");
   const cellElement = document.querySelectorAll("[data-cell]");
-
   const winNum = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
-    [1, 4, 7], 
+    [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6], 
+    [2, 4, 6],
   ];
- 
+
   let playerOne = choice.value;
   let playerTwo = "";
   if (playerOne === "X") {
@@ -48,14 +49,17 @@ const displayController = (() => {
   function makeMove(e) {
     const cell = e.target;
     const currentTurn = !playerTurn ? playerOne : playerTwo;
-    console.log(currentTurn);
-    //   when player clicks a tile, the X or O displays in the tile
     placeMarker(cell, currentTurn);
-    //   switch turns after player chooses a tile
-    swapTurn();
-    turn();
+    if (checkWin(currentTurn)) {
+      endGame(false, currentTurn);
+    } else if (isDraw()) {
+      endGame(true);
+    } else {
+      swapTurn();
+      turn();
+    }
   }
-
+//    display whos turn it is
   function turn() {
     if (!playerTurn) {
       turnPlayer.textContent = `${playerOne} Turn`;
@@ -65,23 +69,44 @@ const displayController = (() => {
     }
   }
 
+  //   switch turns after player chooses a tile
   function swapTurn() {
     playerTurn = !playerTurn;
   }
 
+  //   check to see if theres a winner everytime a tile is chosen
   function checkWin(currentTurn) {
     return winNum.some((combos) => {
       return combos.every((index) => {
-        return cellElement[index].textContent.contains(currentTurn);
+        return cellElement[index].classList.contains(currentTurn);
       });
     });
   }
 
+  //   when player clicks a tile, the X or O displays in the tile
   function placeMarker(cell, currentTurn) {
     cell.textContent = currentTurn;
+    cell.classList = currentTurn;
+  }
+
+  //   if theres a winner, endgame and display who won
+  function endGame(draw, currentTurn) {
+    if (draw) {
+      winText.textContent = "Draw!";
+    } else {
+      winText.textContent = `${currentTurn} Wins!`;
+    }
+    winningMessage.classList.add("show");
+  }
+
+//   if there is no winner when all the tiles are clicked, display a Draw
+  function isDraw() {
+    return [...cellElement].every((cell) => {
+      return (
+        cell.classList.contains(playerOne) || cell.classList.contains(playerTwo)
+      );
+    });
   }
 })();
 
-//   check to see if theres a winner everytime a tile is chosen
-//   if theres a winner, endgame and display who won
-//   if there is no winner when all the tiles are clicked, display a tie
+
